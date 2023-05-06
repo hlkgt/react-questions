4f3bc80fbb36a78856a8e9ee3934c916eba1ca3d
hlkgt <haihaioell@gmail.com>
2023-05-06 15:11:06 +0700

    Matching the answers with the question answers, and generating points

--- reactQuestions/src/App.jsx
+++ reactQuestions/src/App.jsx
@@ -17,32 +17,32 @@ const BoxQuest = ({ id, text, children }) => {
 };
 
 const QuestChoice = (props) => {
-  const { id, choice, textChoice, name } = props;
+  const { id, choice, text, name } = props;
   return (
     <div className="col-span-1">
       <input
         type="radio"
         name={name}
         id={id}
-        value={textChoice}
+        value={choice}
         className="mr-2"
         {...props}
       />
       <label htmlFor={id}>
-        {choice}. {textChoice}
+        {choice}. {text}
       </label>
     </div>
   );
 };
 
 BoxQuest.propTypes = {
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
-  children: PropTypes.func,
+  children: PropTypes.array,
 };
 QuestChoice.propTypes = {
   choice: PropTypes.string.isRequired,
-  textChoice: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
 };
@@ -52,6 +52,7 @@ const App = () => {
   const [questions, setQuestions] = useState([]);
   const [isDisabled, setIsDisabled] = useState(false);
   const [answers, setAnswers] = useState({});
+  let [score, setScore] = useState(0);
 
   const getKategori = (kategori) => setKategori(kategori);
 
@@ -81,8 +82,12 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Menggunakan nilai dari setiap soal
-    console.log("Jawaban:", answers);
+    for (let i = 0; i < questions.length; i++) {
+      if (questions[i].answer === answers["answer" + i]) {
+        setScore((score += questions[i].point));
+      }
+    }
+    setQuestions([]);
   };
 
   return (
@@ -94,7 +99,7 @@ const App = () => {
               <div className="col-span-1 h-32 mt-12 bg-slate-100 rounded-md shadow-md flex justify-center items-center flex-col">
                 <h1 className="font-medium md:text-2xl lg:text-xl">My Score</h1>
                 <p className="text-4xl font-medium md:text-5xl lg:text-2xl">
-                  50
+                  {score}
                 </p>
               </div>
               <div className="col-span-1 h-32 mt-12 bg-slate-100 rounded-md shadow-md flex justify-center items-center flex-col">
@@ -114,7 +119,7 @@ const App = () => {
               }
               onChange={(e) => getKategori(e.target.value)}
             >
-              <option className="hidden">Pilih Kategori Soal</option>
+              <option className="hidden">Pilih Kategori Quest</option>
               <option value="matematika">Matematika</option>
               <option value="ipa">IPA</option>
               <option value="ips">IPS</option>
@@ -127,7 +132,7 @@ const App = () => {
               className="block mx-auto px-12 md:px-40 lg:px-12 py-1 md:py-3 lg:py-1 bg-blue-600 rounded-md shadow-md font-medium md:text-xl lg:text-md text-white hover:bg-blue-400 border-white"
               data-click="generate"
             >
-              Generate Soal
+              Generate Quest
             </button>
           </div>
           <div
@@ -146,32 +151,29 @@ const App = () => {
                       <BoxQuest id={quest.id} text={quest.question}>
                         <QuestChoice
                           choice={"A"}
-                          name={"answer" + quest.id}
-                          textChoice={quest.choice.a}
-                          answerId={quest.id}
+                          name={"answer" + i}
+                          text={quest.choice.a}
                           id={quest.id + quest.choice.a}
                           onChange={handleAnswerChange}
                         />
                         <QuestChoice
                           choice={"B"}
-                          name={"answer" + quest.id}
-                          textChoice={quest.choice.b}
-                          answerId={quest.id}
+                          name={"answer" + i}
+                          text={quest.choice.b}
                           id={quest.id + quest.choice.b}
                           onChange={handleAnswerChange}
                         />
                         <QuestChoice
                           choice={"C"}
-                          name={"answer" + quest.id}
-                          textChoice={quest.choice.c}
-                          answerId={quest.id}
+                          name={"answer" + i}
+                          text={quest.choice.c}
                           id={quest.id + quest.choice.c}
                           onChange={handleAnswerChange}
                         />
                         <QuestChoice
                           choice={"D"}
-                          name={"answer" + quest.id}
-                          textChoice={quest.choice.d}
+                          name={"answer" + i}
+                          text={quest.choice.d}
                           id={quest.id + quest.choice.d}
                           onChange={handleAnswerChange}
                         />
@@ -181,7 +183,9 @@ const App = () => {
                 );
               })
             ) : (
-              <h1>Belum Ada Quest Yang Dipilih</h1>
+              <h1 className="text-lg font-semibold">
+                Belum Ada Quest Yang Dipilih
+              </h1>
             )}
             {questions.length > 0 ? (
               <div className="grid grid-cols-4">
